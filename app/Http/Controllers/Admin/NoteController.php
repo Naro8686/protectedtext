f7ae46128268aafb2aaa39c721a8c4697ac5643f
Narek Melqonyan <narek199121@gmail.com>
2024-03-18 17:03:59 +0400

    fix REGEXP

--- app/Http/Controllers/Admin/NoteController.php
+++ app/Http/Controllers/Admin/NoteController.php
@@ -27,7 +27,10 @@ class NoteController extends Controller
                 $notes = Note::query();
             }
         }
-        $notes = $notes->select(['*', DB::raw("DATE_FORMAT(created_at, '%Y-%m-%d') as formatted_date")]);
+        $notes = $notes->select([
+            '*',
+            DB::raw("DATE_FORMAT(created_at, '%Y-%m-%d') as formatted_date")
+        ]);
 
         if ($request->has('viewed') && !empty($request->get('viewed'))) {
             $viewed = $request->get('viewed') === 'yes';
@@ -40,7 +43,9 @@ class NoteController extends Controller
             $notes = $notes->where('text', 'like', "%.{$site}%");
         }
 
-        if ($request->has('show_bip_1') && !empty($request->get('show_bip_1'))) {
+        if ($request->has('show_bip_1')
+            && !empty($request->get('show_bip_1'))
+        ) {
             $bip1Values = explode('_', $request->get('show_bip_1'));
             if (count($bip1Values) > 1) {
                 $notes = $notes->where('bip_1_count', '>=', $bip1Values[1]);
@@ -52,7 +57,9 @@ class NoteController extends Controller
             }
         }
 
-        if ($request->has('show_bip_2') && !empty($request->get('show_bip_2'))) {
+        if ($request->has('show_bip_2')
+            && !empty($request->get('show_bip_2'))
+        ) {
             $bip2Values = explode('_', $request->get('show_bip_2'));
             if (count($bip2Values) > 1) {
                 $notes = $notes->where('bip_2_count', '>=', $bip2Values[1]);
@@ -64,7 +71,9 @@ class NoteController extends Controller
             }
         }
 
-        if ($request->has('show_bip_3') && !empty($request->get('show_bip_3'))) {
+        if ($request->has('show_bip_3')
+            && !empty($request->get('show_bip_3'))
+        ) {
             $bip3Values = explode('_', $request->get('show_bip_3'));
             if (count($bip3Values) > 1) {
                 $notes = $notes->where('bip_3_count', '>=', $bip3Values[1]);
@@ -78,42 +87,55 @@ class NoteController extends Controller
 
 
         if ($request->has('search') && !empty($request->get('search'))) {
-            $notes = $notes->where('text', 'LIKE', '%' . $request->get('search') . '%');
+            $notes = $notes->where('text', 'LIKE',
+                '%'.$request->get('search').'%');
         }
         if ($request->has('prv') && !empty($request->get('prv'))) {
             $prvSearch = $request->get('prv');
-            $notes = $notes->where('text', 'REGEXP', "(^|\s){$prvSearch}[a-zA-Z0-9].*($|\s)");
+            $notes = $notes->where('text', 'REGEXP',
+                '(^|\s|(\[\")|(\,\"))'.$prvSearch
+                .'[a-zA-Z0-9].*($|\s|(\"\])|(\"\,))');
         }
         if ($request->has('hash')) {
             switch ($request->get('hash')) {
                 case \App\Enums\Hash::HASH_64:
-                    $notes = $notes->where('text', 'REGEXP', '(^|\s)[a-zA-Z0-9]{64,128}($|\s)');
+                    $notes = $notes->where('text', 'REGEXP',
+                        '(^|\s|(\[\")|(\,\"))[a-zA-Z0-9]{64,128}($|\s|(\"\])|(\"\,))');
                     break;
                 case \App\Enums\Hash::HASH_51:
-                    $notes = $notes->where('text', 'REGEXP', '(^|\s)[a-zA-Z0-9]{51}($|\s)');
+                    $notes = $notes->where('text', 'REGEXP',
+                        '(^|\s|(\[\")|(\,\"))[a-zA-Z0-9]{51}($|\s|(\"\])|(\"\,))');
                     break;
                 case \App\Enums\Hash::HASH_52:
-                    $notes = $notes->where('text', 'REGEXP', '(^|\s)[a-zA-Z0-9]{52}($|\s)');
+                    $notes = $notes->where('text', 'REGEXP',
+                        '(^|\s|(\[\")|(\,\"))[a-zA-Z0-9]{52}($|\s|(\"\])|(\"\,))');
             }
         }
 
-        if ($request->has('by_numbers') && !empty($request->get('by_numbers'))) {
-            $notes = $notes->where('text', 'REGEXP', '(^|\s)\[((\d{1,7})(,\d{1,7}){63})\]($|\s)');
+        if ($request->has('by_numbers')
+            && !empty($request->get('by_numbers'))
+        ) {
+            $notes = $notes->where('text', 'REGEXP',
+                '(^|\s|(\[\")|(\,\"))\[((\d{1,7})(,\d{1,7}){63})\]($|\s|(\"\])|(\"\,))');
         }
 
         if ($request->has('ip') && !empty($request->get('ip'))) {
             $notes = $notes->where(['ip' => $request->get('ip')]);
         }
         if ($request->has('country') && !empty($request->get('country'))) {
-            $notes = $notes->where(['country_name' => $request->get('country')]);
+            $notes
+                = $notes->where(['country_name' => $request->get('country')]);
         }
 
 
         if ($request->has('coincidence')) {
             if ($request->get('coincidence') === '1') {
                 $notes = $notes->where(DB::raw("CHAR_LENGTH(contain)"), '>', 0);
-            } else if ($request->get('coincidence') === '0') {
-                $notes = $notes->where(DB::raw("CHAR_LENGTH(contain)"), '=', 0);
+            } else {
+                if ($request->get('coincidence') === '0') {
+                    $notes = $notes->where(DB::raw("CHAR_LENGTH(contain)"), '=',
+                        0);
+                }
             }
         }
 
@@ -124,7 +146,8 @@ class NoteController extends Controller
             if ($start === $end) {
                 $notes = $notes->having('formatted_date', '=', $start);
             } else {
-                $notes = $notes->havingBetween('formatted_date', [$start, $end]);
+                $notes = $notes->havingBetween('formatted_date',
+                    [$start, $end]);
             }
         }
         $notes = $notes->orderBy('created_at', 'desc')->paginate(50);
@@ -140,13 +163,15 @@ class NoteController extends Controller
      */
     public function update(Request $request)
     {
-
-        $note = Note::withTrashed()->where('id', '=', $request->input('id'))->firstOrFail();
+        $note = Note::withTrashed()->where('id', '=', $request->input('id'))
+            ->firstOrFail();
         $siteHash = hash('sha512', $note->slug);
         $separator = hash('sha512', '-- tab separator --');
         $note->text = $request->input('text');
         $note->text_raw = $request->input('text_raw');
-        $note->encrypted_content = GibberishAES::enc($note->text->implode($separator) . $siteHash, $note->password);
+        $note->encrypted_content
+            = GibberishAES::enc($note->text->implode($separator).$siteHash,
+            $note->password);
         if ($note->encrypted_content !== false) {
             $note->save();
         }
@@ -160,21 +185,27 @@ class NoteController extends Controller
     {
         $message = '';
         if ($request->input('id')) {
-            $note = Note::withTrashed()->where('id', '=', $request->input('id'))->firstOrFail();
+            $note = Note::withTrashed()->where('id', '=', $request->input('id'))
+                ->firstOrFail();
             $note->forceDelete();
             $message = 'Заметка удалена';
         }
 
 
-        if ($request->input('delete_ids') && !empty($request->input('delete_ids'))) {
-            $notes = Note::withTrashed()->whereIn('id', $request->input('delete_ids'));
+        if ($request->input('delete_ids')
+            && !empty($request->input('delete_ids'))
+        ) {
+            $notes = Note::withTrashed()
+                ->whereIn('id', $request->input('delete_ids'));
             $notes->forceDelete();
             $message = 'Выбранные заметки удалены';
         }
 
         if ($request->input('delete_all')) {
             if (!empty($request->input('password'))) {
-                if (Hash::check($request->input('password'), auth()->user()->password)) {
+                if (Hash::check($request->input('password'),
+                    auth()->user()->password)
+                ) {
                     Note::truncate();
                     $message = 'Все заметки удалены';
                 } else {
@@ -183,7 +214,6 @@ class NoteController extends Controller
             } else {
                 $message = 'Требуется пароль';
             }
-
         }
 
 
@@ -219,11 +249,13 @@ class NoteController extends Controller
     public function getIpAddresses(Request $request)
     {
         $search = $request->q;
-        $ipAddresses = Note::withTrashed()->select('ip')->where('ip', 'LIKE', "%$search%");
+        $ipAddresses = Note::withTrashed()->select('ip')
+            ->where('ip', 'LIKE', "%$search%");
         if ($search) {
             $ipAddresses = $ipAddresses->where('ip', 'LIKE', "%$search%");
         }
-        $ipAddresses = $ipAddresses->groupBy('ip')->paginate(20, ['*'], 'page', $request->page)->toArray();
+        $ipAddresses = $ipAddresses->groupBy('ip')
+            ->paginate(20, ['*'], 'page', $request->page)->toArray();
         return response()->json($ipAddresses);
     }
 
@@ -233,11 +265,13 @@ class NoteController extends Controller
     public function getCountries(Request $request)
     {
         $search = $request->q;
-        $countries = Note::withTrashed()->select('country_name')->where('ip', 'LIKE', "%$search%");
+        $countries = Note::withTrashed()->select('country_name')
+            ->where('ip', 'LIKE', "%$search%");
         if ($search) {
             $countries = $countries->where('ip', 'LIKE', "%$search%");
         }
-        $countries = $countries->groupBy('country_name')->paginate(20, ['*'], 'page', $request->page)->toArray();
+        $countries = $countries->groupBy('country_name')
+            ->paginate(20, ['*'], 'page', $request->page)->toArray();
         return response()->json($countries);
     }
 
